eaf5b5a72bd2c31a0468015b5148f752affb9a25
wangyuebin <wangyb@ecqun.com>
2017-09-14 10:16:55 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/lljy/wxapi/WXPayEntryActivity.java
+++ app/src/main/java/com/cyanbirds/lljy/wxapi/WXPayEntryActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.cyanbirds.lljy.CSApplication;
+import com.cyanbirds.lljy.R;
 import com.cyanbirds.lljy.activity.base.BaseActivity;
 import com.cyanbirds.lljy.eventtype.PayEvent;
 import com.cyanbirds.lljy.manager.AppManager;
@@ -59,9 +60,9 @@ public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
 		if (baseResp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
 			if (baseResp.errCode == 0) {
 				EventBus.getDefault().post(new PayEvent());
-				ToastUtil.showMessage("支付成功");
+				ToastUtil.showMessage(R.string.pay_success);
 			} else if (baseResp.errCode == -1){
-				ToastUtil.showMessage("支付失败,请重试");
+				ToastUtil.showMessage(R.string.pay_wechat_failure);
 			} else {
 				ToastUtil.showMessage("已取消");
 			}
