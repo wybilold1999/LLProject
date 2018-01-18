507bbb4835e51a43a36e98894ec62ce27b02a19e
wangyuebin <wangyb@ecqun.com>
2018-01-18 20:01:37 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/lljy/config/AppConstants.java
+++ app/src/main/java/com/cyanbirds/lljy/config/AppConstants.java
@@ -12,7 +12,7 @@ public class AppConstants {
 	
 	public static final String BASE_URL = "http://120.76.54.8/LoveLoveServer/";
 //	public static final String BASE_URL = "http://192.168.1.100/LoveLoveServer/";
-//	public static final String BASE_URL = "http://10.0.108.198:8080/LoveLoveServer/";
+//	public static final String BASE_URL = "http://10.0.109.239:8080/LoveLoveServer/";
 
 	/**
 	 * 密码加密密匙
