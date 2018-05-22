5495f5944a1f683f0c4aae7ac32c1ac8f6ae6526
yuebinwang <395044952@qq.com>
2018-05-22 17:40:42 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/lljy/activity/SettingActivity.java
+++ app/src/main/java/com/cyanbirds/lljy/activity/SettingActivity.java
@@ -148,9 +148,7 @@ public class SettingActivity extends BaseActivity {
                 }
                 break;
             case R.id.banding_phone_lay:
-                //0=注册1=找回密码2=验证绑定手机
-                intent.setClass(this, FindPwdActivity.class);
-                intent.putExtra(ValueKey.INPUT_PHONE_TYPE, 2);
+                intent.setClass(this, BandPhoneActivity.class);
                 startActivity(intent);
                 finish();
                 break;
